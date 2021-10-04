ed108ee6ef70589f7295c824dfaaa166769876ef
CHARAN KUMAR UDAYAGIRI <67089878+UDAYAGIRICHARAN@users.noreply.github.com>
2021-10-04 06:48:33 +0530

    eliminate.py
    Given the single linked list above function that removes the consecutive duplicate values, the given list only contains unique elements and returns the head to the updated list.

---
+++ python/eliminate.py
@@ -0,0 +1,89 @@
+from sys import stdin
+
+#Following is the Node class already written for the Linked List
+class Node :
+    def __init__(self, data) :
+        self.data = data
+        self.next = None
+
+
+
+
+def removeDuplicates(head) :
+    #Your code goes here
+    c=head
+    while c and c.next: 
+        
+            if c.data==c.next.data:
+                  c.next=c.next.next
+            else:
+                  c=c.next
+
+    return head
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+#Taking Input Using Fast I/O
+def takeInput() :
+    head = None
+    tail = None
+
+    datas = list(map(int, stdin.readline().rstrip().split(" ")))
+
+    i = 0
+    while (i < len(datas)) and (datas[i] != -1) :
+        data = datas[i]
+        newNode = Node(data)
+
+        if head is None :
+            head = newNode
+            tail = newNode
+
+        else :
+            tail.next = newNode
+            tail = newNode
+
+        i += 1
+
+    return head
+
+
+#to print the linked list 
+def printLinkedList(head) :
+
+    while head is not None :
+        print(head.data, end = " ")
+        head = head.next
+
+    print()
+
+
+#main
+t = int(stdin.readline().strip())
+
+while t > 0 :
+    head = takeInput()
+
+    head = removeDuplicates(head)
+    printLinkedList(head)
+
+    t -= 1
