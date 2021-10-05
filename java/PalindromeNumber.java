ba17701c67a50894db4601a05ad12e78ff51ac13
Otacon94 <gcerzoso@gmail.com>
2021-10-05 23:26:31 +0200

    fixed compilation problems, merged BFS and DFS into Graphs class

--- java/PalindromeNumber.java
+++ java/PalindromeNumber.java
@@ -1,6 +1,5 @@
 import java.util.*;
-public class Main
-{
+public class PalindromeNumber {
 	public static void main(String[] args) {
 		Scanner scan=new Scanner(System.in);
 		int n=scan.nextInt();
